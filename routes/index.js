7846bb2f43a2f03c6da114bd2bb85807cdbb9847
Millelang <millelang2005@gmail.com>
2024-01-23 11:10:30 +0100

    boom

--- routes/index.js
+++ routes/index.js
@@ -33,12 +33,14 @@ router.get('/story/:id', function (req, res) {
   res.render('part.njk', { title: text, part: part })
 })
 
-const pool = require('./db') 
+const pool = require('./db')
 
-router.get('/dbtest', async (req, res) => {
+router.get('/dbtest/:id', async (req, res) => {
   try {
-    const [parts] = await pool.promise().query('SELECT * FROM milton_part')
-    res.json({ parts})
+    const id = req.params.id
+    const [parts] = await pool.promise().query(`SELECT * FROM milton_part WHERE id = ${id}`)
+    const [options] = await pool.promise().query(`SELECT * FROM milton_option WHERE part_id = ${id}`)
+    res.json({ parts, options })
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
